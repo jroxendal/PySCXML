44c89c96e55524040726d8a1fb16af462650df64
Johan Roxendal <johan@roxendal.com>
2009-11-11 18:08:25 +0000

    added the license text to the files.

--- src/scxml/node.py
+++ src/scxml/node.py
@@ -2,6 +2,21 @@
 Created on Nov 7, 2009
 
 @author: Johan Roxendal
+
+This file is part of pyscxml.
+
+    pyscxml is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    pyscxml is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with pyscxml.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
 class SCXMLNode(object):
