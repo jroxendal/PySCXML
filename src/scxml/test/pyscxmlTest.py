3fb131cef4618ee10a6bcff73147c8dc6705c1e3
Johan Roxendal <johan@roxendal.com>
2009-11-20 11:07:56 +0000

    fixed bugs in the build script. it now correctly runs the unittest.

---
+++ src/scxml/test/pyscxmlTest.py
@@ -0,0 +1,47 @@
+'''
+This file is part of pyscxml.
+
+    pyscxml is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    pyscxml is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with pyscxml.  If not, see <http://www.gnu.org/licenses/>.
+    
+    @author: Johan Roxendal
+'''
+
+import unittest
+from scxml.compiler import Compiler
+from xml.etree import ElementTree as etree
+from os import listdir, curdir
+import os.path
+
+class CompilerTest(unittest.TestCase):
+    ''' 
+    This test class is run from the context of the build.xml found in the project root.
+    '''
+    
+    def testCompiler(self):
+        
+        xmlDir = "unittest_xml/"
+        for xmlDoc in [x for x in listdir(xmlDir) if x != ".svn"]:
+            xml = open(xmlDir + xmlDoc).read()
+
+            compiler = Compiler()
+            doc = compiler.parseXML(xml)
+            
+            # make sure that the amount of states stored in the stateDict in the parsed document equals
+            # the amount of xml nodes of the same types.
+            self.assertEqual(len(doc.stateDict.keys()),
+                len(list(state for state in etree.fromstring(xml).getiterator() if state.tag in ["state", "parallel", "final", "history", "scxml"]))
+            )
+            
+        
+    
