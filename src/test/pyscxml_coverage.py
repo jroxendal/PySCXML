746be661ede9ccb1595b5a72a69308f6d1a556c3
Johan Roxendal <johan@roxendal.com>
2010-10-30 00:07:10 +0000

    history test added.

--- src/test/pyscxml_coverage.py
+++ src/test/pyscxml_coverage.py
@@ -6,8 +6,6 @@ Created on Dec 7, 2009
 
 import sys
 import unittest
-print sys.version_info
-print sys.path
 
 import coverage
 import pyscxmlTest
@@ -22,10 +20,63 @@ if not os.path.isdir(xmlDir):
 cov = coverage.coverage()
 cov.start()
 
-unittest.makeSuite(pyscxmlTest.RegressionTest)
-unittest.main()
-#pyscxmlTest.RegressionTest().testInterpreter()
+sm = StateMachine(open(xmlDir + "colors.xml").read())
+sm.start()
+time.sleep(1) #lets us avoid asynchronous errors
 
+sm = StateMachine(open(xmlDir + "parallel.xml").read())
+sm.start()
+time.sleep(1) #lets us avoid asynchronous errors
+
+sm = StateMachine(open(xmlDir + "factorial.xml").read())
+sm.start()
+time.sleep(1) #lets us avoid asynchronous errors
+
+sm = StateMachine(open(xmlDir + "issue_164.xml").read())
+sm.start()
+time.sleep(1) #lets us avoid asynchronous errors
+
+sm = StateMachine(open(xmlDir + "all_configs.xml").read())
+sm.start()
+sm.send("a")
+sm.send("b")
+sm.send("c")
+sm.send("d")
+sm.send("e")
+sm.send("f")
+sm.send("g")
+sm.send("h")
+time.sleep(1) #lets us avoid asynchronous errors
+
+sm = StateMachine(open(xmlDir + "issue_626.xml").read())
+sm.start()
+time.sleep(1) #lets us avoid asynchronous errors
+
+sm = StateMachine(open(xmlDir + "twolock_door.xml").read())
+sm.start()
+time.sleep(1) #lets us avoid asynchronous errors
+'''
+sm = StateMachine(open(xmlDir + "xinclude.xml").read())
+sm.start()
+time.sleep(1) #lets us avoid asynchronous errors
+self.assert_(sm.isFinished())
+'''
+
+sm = StateMachine(open(xmlDir + "if_block.xml").read())
+sm.start()
+time.sleep(1) #lets us avoid asynchronous errors
+
+sm = StateMachine(open(xmlDir + "donedata.xml").read())
+sm.start()
+time.sleep(1) #lets us avoid asynchronous errors
+
+sm = StateMachine(open(xmlDir + "invoke.xml").read())
+sm.start()
+time.sleep(1) #lets us avoid asynchronous errors
+
+sm = StateMachine(open(xmlDir + "history.xml").read())
+sm.start()
+time.sleep(6) #lets us avoid asynchronous errors
 
 cov.stop()
 cov.html_report(directory='coverage_output')
